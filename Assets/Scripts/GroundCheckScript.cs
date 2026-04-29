8be70fb32656bf353c32875f7f6ec01b6d95c577
Saigonese07 <musicjames08@gmail.com>
2026-04-29 09:18:06 +0200

    Slut Lektion 2 - 04/29
    + Skapade player
    + Skapade Platforms
    + Skapade Animations för Mario
    + Player Movement

---
+++ Assets/Scripts/GroundCheckScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class GroundCheckScript : MonoBehaviour
+{
+    public bool isGrounded = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrounded)
+        {
+            Debug.Log("[1] isGrounded = true");
+        }
+        else if (isGrounded == false)
+        {
+            Debug.Log("[1] isGrounded = false");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrounded = false;
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float MoveSpeed = 5;
+    public float JumpSpeed = 5;
+
+    private SpriteRenderer spriteRenderer;
+
+    Animator animator;
+
+    bool isCrouching;
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocityX = Input.GetAxisRaw("Horizontal") * MoveSpeed;
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            rb.linearVelocityY = JumpSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            isCrouching = true;
+        }
+        else
+        {
+            isCrouching = false;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    void HandleAnimations()
+    {
+        if (isCrouching == true)
+        {
+            animator.Play("Player_Crouch");
+            return;
+        }
+        else if (rb.linearVelocity.sqrMagnitude > 0)
+        {
+            animator.Play("Player_Walk");
+        }
+        else if (rb.linearVelocity.sqrMagnitude > 0)
+        {
+            animator.Play("Player_Jump");
+        }
+        else
+        {
+            animator.Play("Player_Idle");
+        }
+    }
+}
