ddacab742d11dc2e44f71349293d05e1b6e7e6e6
Saigonese07 <musicjames08@gmail.com>
2026-05-06 09:18:35 +0200

    Slut lektion - 06/05
    + flag
    + flag animation
    + flagFinish (när Player nuddar flag så åker flaggan ner)
    + level 2
    
    Jag lade till en flag funktion, den har en flag animation. Gjorde även så att flaggen åker ner när man nuddar den med Player. Lade även till level 2 i scenes.

---
+++ Assets/Scripts/flagScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class flagScript : MonoBehaviour
+{
+    public string nextLevel;
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.Play("flagFinish");
+            Invoke("LoadNextLevel", 2f);
+        }
+
+    }
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+}
