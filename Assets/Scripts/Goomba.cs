eecee1a1b2f0c78c3f83af171263778d0327b0bb
Saigonese07 <musicjames08@gmail.com>
2026-05-20 09:21:52 +0200

    Slut lektion - 20/05
    + goomba wallcheck och stompcheck
    
    Skapade en goomba wallacheck och stomcheck samt en stomp check (som GroundCheck) på player prefaben. När båda stomcheck nuddar varandra så dör goomba men en animation. Goomba kan även gå fram och tillbaka vid pipes.

---
+++ Assets/Scripts/Goomba.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Goomba : MonoBehaviour
+{
+    public float speed = 2f;
+
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator anim;
+    BoxCollider2D bc;
+
+    bool dead = false;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        bc = GetComponent<BoxCollider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!dead)
+        {
+            rb.linearVelocity = new Vector2(speed, rb.linearVelocityY);
+        }
+
+    }
+
+    void Flip()
+    {
+        speed *= -1;
+
+        sr.flipX = !sr.flipX;
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (dead) return;
+
+        if (other.CompareTag("Pipe"))
+        {
+            Flip();
+        }
+
+        if (other.CompareTag("Stomp"))
+        {
+            Die();
+
+        }
+    }
+
+    void Die()
+    {
+        dead = true;
+        rb.linearVelocity = new Vector2(0f, rb.linearVelocityY);
+        
+        anim.Play("Goomba_Dead");
+        Debug.Log("Goomba eliminated");
+        bc.isTrigger = false;
+    }
+}
--- Assets/Scripts/GroundCheckScript.cs
+++ Assets/Scripts/GroundCheckScript.cs
@@ -18,7 +18,7 @@ public class GroundCheckScript : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("Ground"))
+        if (collision.CompareTag("Ground") || collision.CompareTag("Pipe") || collision.CompareTag("Enemy"))
         {
             isGrounded = true;
         }
@@ -26,7 +26,7 @@ public class GroundCheckScript : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Ground"))
+        if (collision.CompareTag("Ground") || collision.CompareTag("Pipe")|| collision.CompareTag("Enemy"))
         {
             isGrounded = false;
         }
